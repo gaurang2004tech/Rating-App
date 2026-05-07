f847bb184da29e90c4ff8041cbe685604da03842
Gaurang Raut <Gaurangraut2004@gmail.com>
2026-05-07 12:09:22 +0530

    server fixn

--- rating-app-backend/src/main.ts
+++ rating-app-backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3001);
+  const port = configService.get<number>('PORT', 3000);
 
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
